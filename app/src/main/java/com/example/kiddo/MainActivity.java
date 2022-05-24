27d20780e74062af914368943291ee5c63ddd0d0
Danyal <Daniraj107@gmail.com>
2022-05-23 21:05:43 -0700

    Image Activity Layout

--- app/src/main/java/com/example/kiddo/MainActivity.java
+++ app/src/main/java/com/example/kiddo/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.kiddo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button1,button2,button3,button4,button5,button6,button7,button8,button9,button10;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1=findViewById(R.id.button1);
     }
 }
