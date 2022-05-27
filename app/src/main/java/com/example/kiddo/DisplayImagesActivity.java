6dc43cfb6547f03748b9597dc3ec4dc8d01eb6d5
Danyal <Daniraj107@gmail.com>
2022-05-27 12:44:19 -0700

    Adding Fuctionality

--- app/src/main/java/com/example/kiddo/DisplayImagesActivity.java
+++ app/src/main/java/com/example/kiddo/DisplayImagesActivity.java
@@ -2,13 +2,30 @@ package com.example.kiddo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DisplayImagesActivity extends AppCompatActivity {
 
+    TextView textView;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_images);
+
+        textView=findViewById(R.id.TextViewButtonText);
+        imageView=findViewById(R.id.imageView);
+        Intent intent=getIntent();
+
+        int imageResource=Integer.parseInt(intent.getStringExtra("Image"));
+        String buttonText=intent.getStringExtra("buttonText");
+
+        imageView.setImageResource(imageResource);
+        imageView.setContentDescription(buttonText);
+        textView.setText(buttonText);
+
     }
 }
--- app/src/main/java/com/example/kiddo/MainActivity.java
+++ app/src/main/java/com/example/kiddo/MainActivity.java
@@ -2,18 +2,64 @@ package com.example.kiddo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1,button2,button3,button4,button5,button6,button7,button8,button9,button10;
-    TextView textView;
+
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1=findViewById(R.id.button1);
     }
+    public void OnClickButton(View view)
+    {
+        button=findViewById(view.getId());
+        int imageResource;
+        switch (Integer.parseInt(button.getText().toString())) {
+            case 1:
+                imageResource = R.drawable.one;
+                break;
+            case 2:
+                imageResource = R.drawable.two;
+                break;
+            case 3:
+                imageResource = R.drawable.three;
+                break;
+            case 4:
+                imageResource = R.drawable.four;
+                break;
+            case 5:
+                imageResource = R.drawable.five;
+                break;
+            case 6:
+                imageResource = R.drawable.six;
+                break;
+            case 7:
+                imageResource = R.drawable.seven;
+                break;
+            case 8:
+                imageResource = R.drawable.eight;
+                break;
+            case 9:
+                imageResource = R.drawable.nine;
+                break;
+            default:
+                imageResource = R.drawable.ten;
+                break;
+        }
+        Intent intent=new Intent(MainActivity.this,DisplayImagesActivity.class);
+        intent.putExtra("Image", String.valueOf(imageResource));
+        intent.putExtra("buttonText",button.getText().toString());
+        startActivity(intent);
+    }
+
 }
